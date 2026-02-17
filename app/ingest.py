88a571ad9d0569e20973db2d11f252f1740a011d
sreenumr <sreenu959@gmail.com>
2026-02-17 21:50:24 +0000

    DB command to ingest csv into db

---
+++ app/ingest.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import uuid
+from db import get_connection
+
+def ingest_csv(path : str):
+    df = pd.read_csv(path)
+    conn = get_connection()
+
+    for _, row in df.iterrows():
+        conn.execute("""
+    INSERT INTO events VALUES (? , ? , ? , ? , ? , ? , ? )
+""",(
+        str(uuid.uuid4(),
+        row["incident_ud"]),
+        row["timestamp"],
+        row["source"],
+        row["author"],
+        row["event_type"],
+        row["content"],
+         ))
+    conn. close()
